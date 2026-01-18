5f31f8e421840e9ccb311cb11cee9385cd2e4e3e
igarashi-aki <aki_usui@mail.toyota.co.jp>
2026-01-19 07:27:30 +0900

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,37 @@
+'use strict'
+
+//チェックボックスを取得
+const checkboxBread = document.querySelector("#js-checkbox-bread");
+const checkboxBean = document.querySelector("#js-checkbox-bean");
+const checkboxMeat = document.querySelector("#js-checkbox-meat");
+const checkboxSoup = document.querySelector("#js-checkbox-soup");
+const checkboxBag = document.querySelector("#js-checkbox-bag");
+const checkboxHeart = document.querySelector("#js-checkbox-heart");
+// const checkboxSummer = document.querySelector("#js-checkbox-summer");
+// const checkboxBall = document.querySelector("#js-checkbox-ball");
+
+//配列っぽくリスト状にチェックボックス6個取得
+const checkboxs = document.getElementsByClassName("check-btn");
+
+//ローカルストレージのデータを取り出して、チェックボックスへ反映
+const reloadDateObject = JSON.parse(localStorage.getItem("dochiraka")); //オブジェクト
+for (let i = 0; i < checkboxs.length; i++) {
+  checkboxs[i].checked = reloadDateObject[String(i)];
+}
+
+//更新ボタンを取得
+const addBtn = document.getElementById("js-add-btn");
+
+//クイズ追加関数
+function addQuiz(e) {
+  const checkedDeta = {};  
+
+  for (let i = 0; i < checkboxs.length; i++) {
+    checkedDeta[i] = checkboxs[i].checked;
+  }
+
+  localStorage.setItem("dochiraka", JSON.stringify(checkedDeta));
+}
+
+// 更新ボタンクリック時のイベントリスナー
+addBtn.addEventListener("click", addQuiz);
--- js/script.js
+++ js/script.js
@@ -9,83 +9,64 @@ const quizObjects = [
     correctAnswer: 3,
     image: "images/bread.png",
     quizId: "0",
-    visible: true,
   },
   {
     text: "まめは まめでも、そらとぶ まめは？<br>1: えだまめ　2: そらまめ　3: だいず",
     correctAnswer: 2,
     image: "images/bean.png",
     quizId: "1",
-    visible: false,
   },
   {
     text: "にくは にくでも、やさいの にくは？<br>1: にんにく　2: とりにく　3: ぶたにく",
     correctAnswer: 1,
     image: "images/meat.png",
     quizId: "2",
-    visible: false,
   },
   {
     text: "スープの なかにいる いきものは？<br>1: かめ　2: さかな　3: さめ",
     correctAnswer: 1,
     image: "images/soup.png",
     quizId: "3",
-    visible: false,
-  },
-  {
-    text: "かばんの なかにいる いきものは？<br>1: あり　2: ひよこ　3: かば",
-    correctAnswer: 3,
-    image: "images/bag.png",
-    quizId: "4",
-    visible: false,
-  },
-  {
-    text: "やさしいの なかに はいっているものは？<br>1: やぎ　2: やさい　3: やきそば",
-    correctAnswer: 2,
-    image: "images/heart.png",
-    quizId: "5",
-    visible: false,
   },
   {
     text: "なつに おすすめの たべものは？<br>1: すいか　2: ドーナツ　3: アイス",
     correctAnswer: 2,
     image: "images/summer.png",
-    quizId: "6",
-    visible: true,
+    quizId: "4",
   },
   {
     text: "たまを 5こ あつめると でてくる たべものは？<br>1: いくら　2: ボール　3: たまご",
     correctAnswer: 3,
     image: "images/ball.png",
-    quizId: "7",
-    visible: true,
+    quizId: "5",
   },
 ];
 
-//img要素を追加する場所を取得
+//img要素を表示する場所を取得
 const quizsContainer = document.getElementById("js-quizs-container");
 
 //img要素を追加
+const myObject = JSON.parse(localStorage.getItem("dochiraka")); 
+
 for (let i = 0; i < quizObjects.length; i++) {
-  if (quizObjects[i].visible === true) {
-    const createImg = document.createElement("img");
-    createImg.src = quizObjects[i].image;
-    createImg.className = "questionimg";
-    createImg.dataset.quizId = quizObjects[i].quizId;
-    quizsContainer.appendChild(createImg);
+  if (myObject[String(i)]  === true) {
+  const createImg = document.createElement("img");
+  createImg.src = quizObjects[i].image;  
+  createImg.className = "questionimg";
+  createImg.dataset.quizId = quizObjects[i].quizId;
+  quizsContainer.appendChild(createImg);
   }
 }
 
 //問題文を表示する場所を取得
-const questionDisplay = document.getElementById("js-question-display");
+const questionDisplay = document.getElementById("js-question-display"); 
 
-//今解いているクイズオブジェクトを格納する変数の初期化（判定に使用）
-let currentQuestion; 
+//今解いているクイズオブジェクトを変数に入れる用　あとで判定に使う
+let currentQuestion = {}; //初期化
 
 //問題文を表示させる関数
 function makeQuestion(event) {  
   clearQuestionAndAnswer();
-  questionDisplay.style.backgroundColor = "white";  
   currentQuestion = quizObjects[Number(this.dataset.quizId)];
   questionDisplay.innerHTML = currentQuestion.text;
 }
