ef695447b805722305713f4899f653276e45797e
igarashi-aki <aki_usui@mail.toyota.co.jp>
2026-01-19 08:14:07 +0900

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -1,17 +1,22 @@
 'use strict'
-
 //チェックボックスを取得
 const checkboxBread = document.querySelector("#js-checkbox-bread");
 const checkboxBean = document.querySelector("#js-checkbox-bean");
 const checkboxMeat = document.querySelector("#js-checkbox-meat");
 const checkboxSoup = document.querySelector("#js-checkbox-soup");
-const checkboxBag = document.querySelector("#js-checkbox-bag");
-const checkboxHeart = document.querySelector("#js-checkbox-heart");
-// const checkboxSummer = document.querySelector("#js-checkbox-summer");
-// const checkboxBall = document.querySelector("#js-checkbox-ball");
+// const checkboxBag = document.querySelector("#js-checkbox-bag");
+// const checkboxHeart = document.querySelector("#js-checkbox-heart");
+const checkboxSummer = document.querySelector("#js-checkbox-summer");
+const checkboxBall = document.querySelector("#js-checkbox-ball");
 
 //配列っぽくリスト状にチェックボックス6個取得
 const checkboxs = document.getElementsByClassName("check-btn");
+
+//初期設定
+if (!localStorage.getItem("dochiraka")) {
+  const checkedDeta = {"0": true, "2": true, "3": true, "4": false, "5": false, "6": false};
+  localStorage.setItem("dochiraka", JSON.stringify(checkedDeta));
+}
 
 //ローカルストレージのデータを取り出して、チェックボックスへ反映
 const reloadDateObject = JSON.parse(localStorage.getItem("dochiraka")); //オブジェクト
