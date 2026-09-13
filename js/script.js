8eab903bd6b596b7e867eb72e84eb94199ef79e7
aki-igarashi <aki_usui@mail.toyota.co.jp>
2026-09-13 22:34:42 +0900

    解答方式をボタンからテキストクリックへ変更

--- js/script.js
+++ js/script.js
@@ -5,38 +5,56 @@
 //クイズオブジェクト管理
 const quizObjects = [
   {
-    text: "パンはパンでも、はなしを きいてくれる パンは？<br>1: あんぱん　2: しょくぱん　3: カレーパン",
+    text: "パンはパンでも、話を きいてくれる パンは？",
     correctAnswer: 2,
+    choice1: "あんぱん",
+    choice2: "しょくぱん",
+    choice3: "カレーパン",
     image: "images/croissant.png",
     quizId: "0",
   },
   {
-    text: "まめは まめでも、そらとぶ まめは？<br>1: えだまめ　2: そらまめ　3: だいず",
+    text: "まめは まめでも、そらとぶ まめは？",
     correctAnswer: 2,
+    choice1: "えだまめ",
+    choice2: "そらまめ",
+    choice3: "だいず",
     image: "images/bean.png",
     quizId: "1",
   },
   {
-    text: "にくは にくでも、やさいの にくは？<br>1: にんにく　2: とりにく　3: ぶたにく",
+    text: "にくは にくでも、やさいの にくは？",
     correctAnswer: 1,
+    choice1: "にんにく",
+    choice2: "とりにく",
+    choice3: "ぶたにく",
     image: "images/meat.png",
     quizId: "2",
   },
   {
-    text: "スープの なかにいる いきものは？<br>1: かめ　2: さかな　3: さめ",
+    text: "スープの なかにいる いきものは？<br>（ ヒント・・・ わかめスープです😋 ）",
     correctAnswer: 1,
+    choice1: "かめ",
+    choice2: "さかな",
+    choice3: "さめ",
     image: "images/soup.png",
     quizId: "3",
   },
   {
-    text: "なつに おすすめの たべものは？<br>1: すいか　2: ドーナツ　3: アイス",
+    text: "なつに おすすめの たべものは？",
     correctAnswer: 2,
+    choice1: "すいか",
+    choice2: "ドーナツ",
+    choice3: "アイス",
     image: "images/summer.png",
     quizId: "4",
   },
   {
-    text: "たまを 5こ あつめると でてくる たべものは？<br>1: いくら　2: ボール　3: たまご",
+    text: "たまを 5こ あつめると でてくる たべものは？",
     correctAnswer: 3,
+    choice1: "いくら",
+    choice2: "ボール",
+    choice3: "たまご",
     image: "images/ball.png",
     quizId: "5",
   },
@@ -73,6 +91,10 @@ for (let i = 0; i < quizObjects.length; i++) {
 
 //問題文を表示する場所を取得
 const questionDisplay = document.getElementById("js-question-display");
+//解答選択肢を表示する場所を取得
+const choiceDisplay1 = document.getElementById("js-choice-display1");
+const choiceDisplay2 = document.getElementById("js-choice-display2");
+const choiceDisplay3 = document.getElementById("js-choice-display3");
 
 //今解いているクイズオブジェクトを変数に入れる用　あとで判定に使う
 let currentQuestion = {}; //初期化
@@ -84,22 +106,41 @@ function makeQuestion(event) {
   questionDisplay.innerHTML = currentQuestion.text;
 }
 
+// 解答選択肢を表示させる関数
+function makeChoices(event) {
+  // クリアーする関数を作って、呼ぶ。後で。
+
+  currentQuestion = quizObjects[Number(this.dataset.quizId)];
+  choiceDisplay1.innerHTML = currentQuestion.choice1;
+  choiceDisplay1.setAttribute("id", "choice1");
+  choiceDisplay1.dataset.choiceId = "1";
+
+  choiceDisplay2.innerHTML = currentQuestion.choice2;
+  choiceDisplay2.setAttribute("id", "choice2");
+  choiceDisplay2.dataset.choiceId = "2";
+
+  choiceDisplay3.innerHTML = currentQuestion.choice3;
+  choiceDisplay3.setAttribute("id", "choice3");
+  choiceDisplay3.dataset.choiceId = "3";
+}
 //画像クリック時のイベントリスナー
 document.querySelectorAll("img.questionimg").forEach((element) => {
   element.addEventListener("click", makeQuestion);
+  element.addEventListener("click", makeChoices);
 });
 
 //ヒントを表示
-const hintBtn = document.getElementById("js-hint-btn");
-const hintAlert = () => alert("スープは、わかめスープです。");
-hintBtn.addEventListener("click", hintAlert);
+// const hintBtn = document.getElementById("js-hint-btn");
+// const hintAlert = () => alert("スープは、わかめスープです。");
+// hintBtn.addEventListener("click", hintAlert);
 
 //回答エリア＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 //判定結果を表示する場所を取得
 const answerDisplay = document.getElementById("js-answer-display");
 
 //選択された回答番号を格納する変数の初期化
-let currentAnswerBtn;
+// let currentAnswerBtn;
+let currentAnswerText;
 
 //正誤判定関数
 function judgeQ(event) {
@@ -114,8 +155,8 @@ function judgeQ(event) {
     return;
   }
 
-  currentAnswerBtn = Number(this.dataset.btnId);
-  if (currentQuestion.correctAnswer === currentAnswerBtn) {
+  currentAnswerText = Number(this.dataset.choiceId);
+  if (currentQuestion.correctAnswer === currentAnswerText) {
     answerDisplay.textContent = "〇";
     answerDisplay.style.color = "green";
     const sound = new Audio("./sounds/correct.mp3");
@@ -129,7 +170,7 @@ function judgeQ(event) {
 }
 
 //回答ボタンクリック時のイベントリスナー
-document.querySelectorAll(".answer-btn").forEach((btn) => {
+document.querySelectorAll(".answer-choice").forEach((btn) => {
   btn.addEventListener("click", judgeQ);
 });
 
@@ -139,10 +180,13 @@ const clearBtn = document.getElementById("js-clear-btn");
 //回答ディスプレイをクリアーする関数
 function clearQuestionAndAnswer() {
   currentQuestion = "";
-  currentAnswerBtn = "";
+  currentAnswerText = "";
   questionDisplay.textContent = "";
-  answerDisplay.textContent = "";
   questionDisplay.style.backgroundColor = "white";
+  choiceDisplay1.innerHTML = "";
+  choiceDisplay2.innerHTML = "";
+  choiceDisplay3.innerHTML = "";
+  answerDisplay.textContent = "";
 }
 
 //クリアーボタンクリック時のイベントリスナー
